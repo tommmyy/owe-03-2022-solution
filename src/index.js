39bb643520c7203423121d0ca2e25260180f7cac
Tomáš Konrády <tomaskonrady@gmail.com>
2022-03-01 22:05:16 +0100

    demo2 - setup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 // import App from "./Ex1Solution";
-import App from "./Demo1Ex1";
+// import App from "./Demo1Ex1";
 // import App from "./Demo1Ex1Solution";
+import App from "./Demo2";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
